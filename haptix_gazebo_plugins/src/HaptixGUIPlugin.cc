886336832a2fb7fa88e02b21ae44893ab99b7ee7
Brian Gerkey <gerkey@osrfoundation.org>
2014-11-05 18:15:36 -0800

    Minor improvements to initial task setting:
    * Don't select a disabled task.
    * In addition to selecting the task, select the tab containing it.
    * Check for having the initial element before trying to read it (to avoid debug
    warnings)

--- haptix_gazebo_plugins/src/HaptixGUIPlugin.cc
+++ haptix_gazebo_plugins/src/HaptixGUIPlugin.cc
@@ -616,6 +616,7 @@ void HaptixGUIPlugin::InitializeTaskView(sdf::ElementPtr _elem)
     groupFrame->setLayout(groupLayout);
 
     int count = 0;
+    bool initialTab = false;
 
     // Process each task in the group
     while (task)
@@ -659,11 +660,13 @@ void HaptixGUIPlugin::InitializeTaskView(sdf::ElementPtr _elem)
       }
 
       this->taskList[taskIndex] = taskButton;
-      if (task->Get<int>("initial") == 1)
+      if (enabled &&
+          (task->HasElement("initial")) && (task->Get<int>("initial") == 1))
       {
         this->currentTaskId = taskIndex;
         taskButton->setChecked(true);
         this->startStopButton->setDisabled(false);
+	initialTab = true;
       }
 
       task = task->GetNextElement();
@@ -673,6 +676,8 @@ void HaptixGUIPlugin::InitializeTaskView(sdf::ElementPtr _elem)
     }
 
     this->taskTab->addTab(groupFrame, QString::fromStdString(taskGroupName));
+    if (initialTab)
+      this->taskTab->setCurrentIndex(groupIndex);
     taskGroup = taskGroup->GetNextElement("task_group");
     groupIndex++;
   }
