1293bfd182ddc3362bf2ce1b655dd2fdd7451f53
Jackie Kay jacquelinekay1 <Jackie Kay jacquelinekay1@gmail.com>
2015-04-22 14:47:56 -0700

    Feedback

--- cmake/TestUtils.cmake
+++ cmake/TestUtils.cmake
@@ -21,7 +21,7 @@ macro (handsim_build_tests)
       ${CMAKE_BINARY_DIR}/test/libserver_fixture.a
       pthread
       HaptixTracking
-      haptix-comm
+      ${HAPTIX-COMM-LIBRARIES}
       gazebo_common
       gazebo_math
       gazebo_physics
@@ -29,7 +29,10 @@ macro (handsim_build_tests)
       gazebo_rendering
       gazebo_msgs
       gazebo_transport
-      ${Boost_LIBRARIES}
+      ${Boost_FILESYSTEM_LIBRARIES}
+      ${Boost_SYSTEM_LIBRARIES}
+      ${Boost_THREAD_LIBRARIES}
+      ${IGNITION-TRANPOSRT_LIBRARIES}
       )
 
     add_test(${BINARY_NAME} ${CMAKE_CURRENT_BINARY_DIR}/${BINARY_NAME}
