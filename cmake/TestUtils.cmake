a7d0e7c88c95bfbdc68a5b186782659815cbe822
Jackie Kay jacquelinekay1 <Jackie Kay jacquelinekay1@gmail.com>
2015-04-24 11:04:55 -0700

    Integrated with gazebo_test_fixture

--- cmake/TestUtils.cmake
+++ cmake/TestUtils.cmake
@@ -12,16 +12,16 @@ macro (handsim_build_tests)
     add_dependencies(${BINARY_NAME}
       lib${PROJECT_NAME_LOWER}
       gtest gtest_main
-      server_fixture
       )
 
     target_link_libraries(${BINARY_NAME}
       libgtest.a
       libgtest_main.a
-      ${CMAKE_BINARY_DIR}/test/libserver_fixture.a
+      #${CMAKE_BINARY_DIR}/test/libserver_fixture.a
       pthread
       HaptixTracking
       ${HAPTIX-COMM-LIBRARIES}
+      gazebo_test_fixture
       gazebo_common
       gazebo_math
       gazebo_physics
