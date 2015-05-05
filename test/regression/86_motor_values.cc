558ebccb8e9b0e5541659d7981cd7790d4e95a79
Jackie Kay <jacquelinekay1@gmail.com>
2015-05-05 15:50:34 -0700

    First test case

--- test/regression/86_motor_values.cc
+++ test/regression/86_motor_values.cc
@@ -16,6 +16,7 @@
 */
 
 #include <gtest/gtest.h>
+#include <haptix/comm/haptix.h>
 
 #include "gazebo/physics/World.hh"
 #include "gazebo/physics/Model.hh"
@@ -42,7 +43,42 @@ TEST_F(Issue86Test, MotorLimits)
 {
   gazebo::physics::WorldPtr world = this->InitWorld("worlds/arat_test.world");
   ASSERT_TRUE(world != NULL);
-  hx_robot_info();
+
+  // Get joint limits indexed on the numerical motor index
+  gazebo::physics::ModelPtr armModel = world->GetModel("mpl_haptix_right_forearm");
+  ASSERT_TRUE(armModel != NULL);
+
+  sdf::ElementPtr modelSDF = armModel->GetSDF();
+  ASSERT_TRUE(modelSDF != NULL);
+  // TODO: will have to change if multiple ModelPlugins
+  sdf::ElementPtr pluginSDF = modelSDF->GetElement("plugin");
+
+  ASSERT_TRUE(pluginSDF != NULL);
+
+  std::map<int, std::pair<double, double>> indexMotorLimitMap;
+  for (sdf::ElementPtr motorSDF = pluginSDF->GetElement("motor"); motorSDF;
+        motorSDF = pluginSDF->GetNextElement("motor"))
+  {
+    ASSERT_TRUE(motorSDF->HasAttribute("id"));
+    int id = motorSDF->Get<int>("id");
+    ASSERT_TRUE(motorSDF->HasElement("powered_motor_joint"));
+    std::string jointName =
+        motorSDF->GetElement("powered_motor_joint")->Get<std::string>();
+    gazebo::physics::JointPtr joint = armModel->GetJoint(jointName);
+    indexMotorLimitMap[id] =
+        std::pair<double, double>(joint->GetLowerLimit(0).Radian(),
+                                  joint->GetUpperLimit(0).Radian());
+  }
+
+  hxRobotInfo robot_info;
+  ASSERT_EQ(hx_robot_info(&robot_info), hxOK);
+
+  // Expect motor limits to equal limits from SDF
+  for (int i = 0; i < robot_info.motor_count; i++)
+  {
+    EXPECT_DOUBLE_EQ(robot_info.motor_limit[i][0], indexMotorLimitMap[i].first);
+    EXPECT_DOUBLE_EQ(robot_info.motor_limit[i][1], indexMotorLimitMap[i].second);
+  }
 }
 
 /////////////////////////////////////////////////
