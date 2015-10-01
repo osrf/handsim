92c33df4cda90a869af7591b410a559f18b237fc
Carlos Agüero <caguero@osrfoundation.org>
2015-10-01 12:28:02 -0700

    Create a random partition name for testing.

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -7,6 +7,7 @@ include (FindBoost)
 
 find_package(gazebo 7.0.0 REQUIRED)
 find_package(haptix-comm REQUIRED)
+find_package(ignition-math2 REQUIRED)
 find_package(ignition-transport REQUIRED)
 find_package(Boost 1.54.0 COMPONENTS system filesystem thread REQUIRED)
 
